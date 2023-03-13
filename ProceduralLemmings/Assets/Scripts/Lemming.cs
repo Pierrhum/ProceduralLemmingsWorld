ec190fddd667b32161bae056c7d7370ded301ebc
Pierrhum <34352456+Pierrhum@users.noreply.github.com>
2023-03-13 02:25:16 +0100

    Lemming movement anims WIP

--- ProceduralLemmings/Assets/Scripts/Lemming.cs
+++ ProceduralLemmings/Assets/Scripts/Lemming.cs
@@ -6,37 +6,53 @@ using UnityEngine;
 public class Lemming : MonoBehaviour
 {
     private Animator _animator;
+    private int _biome = -1;
     private List<Vector3> _path;
     private int PathIndex = 0;
+    private float Speed = .5f;
     
     void Awake()
     {
         _animator = GetComponent<Animator>();
     }
 
-    public void SetBiome(int biome, List<Vector3> path=null)
+    public void SetBiome(int biome, List<Vector3> path)
     {
+        _biome = biome;
         _animator.SetInteger("Biome", biome);
-        _animator.SetBool("Static", path==null);
-        if (path != null)
+        _animator.SetBool("Static", path.Count > 0);
+        if (path.Count > 0)
         {
             _path = path;
-            //StartCoroutine(MoveCoroutine());
+            StartCoroutine(MoveCoroutine());
         }
     }
 
     private IEnumerator MoveCoroutine()
     {
-        Vector3 initPos = transform.position;
-        Vector3 destination = _path[PathIndex];
-        float timer = 0f;
-        while (transform.position != destination)
+        while (true)
         {
-            //transform.position = Vector3.Lerp(initPos, destination,);
-            yield return new WaitForSeconds(Time.deltaTime);
+            Vector3 initPos = transform.position;
+            Vector3 destination = _path[PathIndex];
+            float Distance = Vector3.Distance(initPos, destination) / Speed;
+            float timer = 0f;
+            // Look At destination
+            while (Vector3.Distance(transform.position, destination) > 0.05f)
+            {
+                transform.position = Vector3.Lerp(initPos, destination, timer / (Distance * 5));
+                timer++;
+                yield return new WaitForSeconds(Time.deltaTime);
+            }
+            PathIndex++;
+            if (PathIndex >= _path.Count) PathIndex = 0;
+            if (_biome != (int)Biome.Desert && _biome != (int)Biome.Snow)
+            {
+                _animator.SetBool("Static", true);
+                yield return new WaitForSeconds(1f);
+                _animator.SetBool("Static", false);
+            }
         }
 
-        PathIndex++;
-        yield return StartCoroutine(MoveCoroutine());
+        yield return null;
     }
 }
--- ProceduralLemmings/Assets/Scripts/SpownLemming.cs
+++ ProceduralLemmings/Assets/Scripts/SpownLemming.cs
@@ -30,21 +30,42 @@ public class SpownLemming : MonoBehaviour
         
         seedGenerator.GenerateSeed();
         List<Vector3> pointTreeList = seedGenerator.GetWorldPointInBiome(biome);
+        List<Vector3> spawnSpoints = pointTreeList;
 
-        if(pointTreeList.Count > 0)
+        if(spawnSpoints.Count > 0)
             for (int i = 0; i < number; i++)
             {
-                int random = Random.Range(0, pointTreeList.Count);
-                AddLemming(pointTreeList[random], biome);
+                int spawn = Random.Range(0, spawnSpoints.Count);
+                List<Vector3> path = new List<Vector3>();
+                // Generate a path for half of lemmings
+                if (i % 2 == 0)
+                {
+                    for (int p = 0; p < 5; p++)
+                    {
+                        int tries=0;
+                        int random = Random.Range(0, pointTreeList.Count);
+                        while (Vector3.Distance(spawnSpoints[spawn], pointTreeList[random]) > 10f)
+                        {
+                            random = Random.Range(0, pointTreeList.Count);
+                            tries++;
+                            if (tries > 200) break;
+                        }
+                        if(tries < 200)
+                            path.Add(pointTreeList[random]);
+                    }
+                }
+                AddLemming(spawnSpoints[spawn], biome, path);
+                spawnSpoints.RemoveAt(spawn);
+                if (spawnSpoints.Count <= 0) break;
             }
     }
    
-    private void AddLemming(Vector3 position, int biome) {
+    private void AddLemming(Vector3 position, int biome, List<Vector3> path) {
         GameObject lemming = Instantiate(Lemming, transform);
         if (biome == (int)Biome.Ocean)
             position -= new Vector3(0,lemming.transform.localScale.y / 2,0);
         lemming.transform.position = position;
         lemming.transform.rotation = Quaternion.Euler(0, Random.Range(0, 359), 0);
-        lemming.GetComponent<Lemming>().SetBiome(biome);
+        lemming.GetComponent<Lemming>().SetBiome(biome, path);
     }
 }
